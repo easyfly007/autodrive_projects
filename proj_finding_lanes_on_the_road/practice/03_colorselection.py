559c3659b3988bda20021b82cfb4b104b4bca50d
easyfly007 <easyfly.huang@gmail.com>
2017-07-29 10:13:53 +0800

    fix a typo

--- proj_finding_lanes_on_the_road/practice/03_colorselection.py
+++ proj_finding_lanes_on_the_road/practice/03_colorselection.py
@@ -4,21 +4,18 @@ import numpy as np
 
 image = mpimg.imread('test.jpg')
 print('this image is: ', type(image),
-	'with dimentions:', image.shape)
+	'with dimensions:', image.shape)
 
 ysize = image.shape[0]
 xsize = image.shape[1]
 color_select = np.copy(image)
-print(color_select)
+
 red_threshold = 220
 green_threshold = 220
 blue_threshold = 220
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-thresholds = (image[:,:, 0] < rgb_threshold[0]) \
-	| (image[:,:,1] < rgb_threshold[1]) \
-	| (image[:,:,2] < rgb_threshold[2])
-print(thresholds)
+thresholds = (image[:,:, 0] < rgb_threshold[0]) 	| (image[:,:,1] < rgb_threshold[1]) 	| (image[:,:,2] < rgb_threshold[2])
 color_select[thresholds] = [0,0,0]
 
 plt.imshow(color_select)
