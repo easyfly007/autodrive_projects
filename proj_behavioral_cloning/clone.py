b0e0864075fe8942be525d23938394d68a9274df
easyfly007 <easyfly.huang@gmail.com>
2017-09-03 15:33:23 +0800

    add lenet for train model

--- proj_behavioral_cloning/clone.py
+++ proj_behavioral_cloning/clone.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout
 
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -29,8 +30,22 @@ y_train = np.array(measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape = (160, 320, 3)))
+
+model.add(Convolution2D(6, 5, 5, border_mode = 'valid'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(16, 5, 5, border_mode = 'valid'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
 model.add(Flatten())
+model.add(Dense(84))
+model.add(Activation('relu'))
+
 model.add(Dense(1))
+
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 7)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
 model.save('model.h5')
