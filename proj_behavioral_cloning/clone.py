c790e158d15e123e5cf47b098825cc40ced8be7d
easyfly007 <easyfly.huang@gmail.com>
2017-09-03 19:58:48 +0800

    add the more complicated net from nv

--- proj_behavioral_cloning/clone.py
+++ proj_behavioral_cloning/clone.py
@@ -58,21 +58,35 @@ model = Sequential()
 model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping = ((70, 25), (0, 0))))
 
-model.add(Convolution2D(6, 5, 5, border_mode = 'valid'))
+model.add(Convolution2D(24, 5, 5, border_mode = 'valid'))
 model.add(MaxPooling2D(pool_size = (2,2)))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(16, 5, 5, border_mode = 'valid'))
+model.add(Convolution2D(36, 5, 5, border_mode = 'valid'))
 model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(48, 5, 5, border_mode = 'valid'))
+# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(64, 3, 3, border_mode = 'valid'))
+# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Activation('relu'))
+
+
+model.add(Convolution2D(64, 3, 3, border_mode = 'valid'))
+# model.add(MaxPooling2D(pool_size = (2,2)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(84))
+
+model.add(Dense(100))
 model.add(Activation('relu'))
 
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
 model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
-model.save('model.h5')
+model.save('model_2.h5')
