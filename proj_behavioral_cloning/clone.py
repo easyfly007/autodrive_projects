85edd6eb5a6de1c28bd0cbab13a3e7b20639c728
easyfly007 <easyfly.huang@gmail.com>
2017-09-03 16:29:53 +0800

    add the more cameras

--- proj_behavioral_cloning/clone.py
+++ proj_behavioral_cloning/clone.py
@@ -16,14 +16,33 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-	fullname = line[0]
 	# as the image is from windows, use \\ as separator
+	measurement = float(line[3])
+	measurements.append(measurement)
+
+	# center 
+	fullname = line[0]
 	filename = fullname.split('\\')[-1]
 	current_path = './data/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+
+	#left camera
+	measurements.append(measurement + 0.2)
+	fullname = line[1]
+	filename = fullname.split('\\')[-1]
+	current_path = './data/IMG/' + filename
+	image = cv2.imread(current_path)
+	images.append(image)
+
+	#right camera
+	measurements.append(measurement - 0.2)
+	fullname = line[1]
+	filename = fullname.split('\\')[-1]
+	current_path = './data/IMG/' + filename
+	image = cv2.imread(current_path)
+	images.append(image)
+
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -54,5 +73,5 @@ model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
 model.save('model.h5')
