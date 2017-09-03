b1e2fe7b26090f080bd0b0b813221f8f5a0ca28c
easyfly007 <easyfly.huang@gmail.com>
2017-09-03 15:58:47 +0800

    do data augmentation by flip the img

--- proj_behavioral_cloning/clone.py
+++ proj_behavioral_cloning/clone.py
@@ -25,8 +25,15 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image, 1))
+	augmented_measurements.append(measurement * (-1.0))
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape = (160, 320, 3)))
@@ -47,5 +54,5 @@ model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
 model.save('model.h5')
