17c45fa5d7ac971f87e73259f6e48af1051c0a75
Yifei Huang <yfhuang@internal.synopsys.com>
2017-09-09 12:25:31 +0800

    update the model by nvidia DAVE2 paper model

--- proj_behavioral_cloning/model.py
+++ proj_behavioral_cloning/model.py
@@ -5,15 +5,16 @@ import os
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout
+from keras.layers.convolutional import Conv2D
+
 from load_data import loadImages
 
-images1, measurements1 = loadImages('./data1')
-images2, measurements2 = loadImages('./data2')
-images3, measurements3 = loadImages('./data3')
+images1, measurements1 = loadImages('./data')
+# images2, measurements2 = loadImages('./data2')
 
-images, measurements = images1 + images2 + images3, measurements1 + measurements2 + measurements3
-# images, measurements = images1, measurements1
-# images, measurements = images3, measurements3
+# images, measurements = images1 + images2, measurements1 + measurements2
+images, measurements = images1, measurements1
+
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -29,39 +30,34 @@ model = Sequential()
 model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping = ((70, 25), (0, 0))))
 
-model.add(Convolution2D(24, 5, 5, border_mode = 'valid'))
-model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Conv2D(24, 5, strides = (2,2), border_mode = 'valid'))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(36, 5, 5, border_mode = 'valid'))
-model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Conv2D(36, 5, strides = (2,2), border_mode = 'valid'))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(48, 5, 5, border_mode = 'valid'))
-# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Conv2D(48, 5, strides = (2,2), border_mode = 'valid'))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(64, 3, 3, border_mode = 'valid'))
-# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Conv2D(64, 3, strides = (1,1), border_mode = 'valid'))
 model.add(Activation('relu'))
 
-
-model.add(Convolution2D(64, 3, 3, border_mode = 'valid'))
-# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Conv2D(64, 3, strides = (1,1), border_mode = 'valid'))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
 
 model.add(Flatten())
 
 model.add(Dense(100))
 model.add(Activation('relu'))
 
-# model.add(Dense(50))
-# model.add(Activation('relu'))
+model.add(Dense(50))
+model.add(Activation('relu'))
 
+model.add(Dense(10))
+model.add(Activation('relu'))
 
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 1)
-model.save('model_01.h5')
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
+model.save('model_3.h5')
